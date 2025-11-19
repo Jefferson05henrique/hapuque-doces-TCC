864a22743665e160dcf9965ecfffe6e8ecdcf688
Esther Correa Carlos Cardote <estheer.correa@hotmail.com>
2025-11-19 07:02:46 -0300

    alterado link sobre nos no header para inicio, ajustes de indentação

--- javascript/busca.js
+++ javascript/busca.js
@@ -1,14 +1,14 @@
 function filtrarCardapio() {
-            const termoBusca = document.getElementById('campoBusca').value.toLowerCase();
-            const itensCardapio = document.querySelectorAll('.doce');
-            
-            itensCardapio.forEach(item => {
-                const textoDoItem = item.textContent.toLowerCase();
-                
-                if (textoDoItem.includes(termoBusca)) {
-                    item.style.display = 'block'; 
-                } else {
-                    item.style.display = 'none'; 
-                }
-            });
-        }
+  const termoBusca = document.getElementById("campoBusca").value.toLowerCase();
+  const itensCardapio = document.querySelectorAll(".doce");
+
+  itensCardapio.forEach((item) => {
+    const textoDoItem = item.textContent.toLowerCase();
+
+    if (textoDoItem.includes(termoBusca)) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
--- javascript/carrousel.js
+++ javascript/carrousel.js
@@ -1,19 +1,19 @@
-const carrossel = document.querySelector('.carrossel-inner'); 
-const slides = document.querySelectorAll('.slide');          
-let indiceAtual = 0;                                       
+const carrossel = document.querySelector(".carrossel-inner");
+const slides = document.querySelectorAll(".slide");
+let indiceAtual = 0;
 
-const tempoDeTroca = 5000;                                 
+const tempoDeTroca = 5000;
 
 function moverCarrossel() {
   indiceAtual++;
-  
+
   if (indiceAtual >= slides.length) {
     indiceAtual = 0;
   }
-  
-  const percentualSlide = 100 / slides.length; 
+
+  const percentualSlide = 100 / slides.length;
   const deslocamentoCorreto = -indiceAtual * percentualSlide;
-  
+
   carrossel.style.transform = `translateX(${deslocamentoCorreto}%)`;
 }
 
